ab5608e8267880326780d99613e97ee6ffb3a999
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-07-17 11:14:03 +0800

    Chane folder name

---
+++ ruby/fibs.rb
@@ -0,0 +1,10 @@
+# Write a function, fibs(num) which returns the first n elements 
+# from the fibonnacci sequence, given n.
+
+# Solve it both iteratively and recursively.
+
+def fibs(num)
+end
+
+def fibs_rec(num)
+end
