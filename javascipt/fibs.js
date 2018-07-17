b80e7c6d83a669fd62a146fa0b5eaeda1b7403cf
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-07-17 11:13:49 +0800

    Add JS version

---
+++ javascipt/fibs.js
@@ -0,0 +1,12 @@
+// Write a function, fibs(num) which returns the first n elements 
+// from the fibonnacci sequence, given n.
+
+// Solve it both iteratively and recursively.
+
+function fibs(num) {
+
+}
+
+function fibs_rec(num) {
+
+}
