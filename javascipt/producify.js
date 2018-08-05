83a745086b8a6e12f76917e6423aa8aa121a2076
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-05 13:32:45 -0700

    Add producify

---
+++ javascipt/producify.js
@@ -0,0 +1,7 @@
+// Given a list of numbers in an array, replace all the
+// numberswith the product of all other numbers.Do this
+// in O(n) time without using division.
+
+function producify(arr) {
+
+};
---
+++ ruby/producify.rb
@@ -0,0 +1,6 @@
+# Given a list of numbers in an array, replace all the
+# numberswith the product of all other numbers.Do this
+# in O(n) time without using division.
+
+def producify(arr)
+end
