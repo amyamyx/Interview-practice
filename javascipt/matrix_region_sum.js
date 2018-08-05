8c74f5e4a8486b37afada3bc10841c369149078e
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-05 13:28:22 -0700

    Add matrix_region_sum

---
+++ javascipt/matrix_region_sum.js
@@ -0,0 +1,7 @@
+// Given a matrix of integers and coordinates of a rectangular
+// region within the matrix, find the sum of numbers falling
+// inside the rectangle.
+
+function mrs(arr) {
+
+};
---
+++ ruby/matrix_region_sum.rb
@@ -0,0 +1,6 @@
+# Given a matrix of integers and coordinates of a rectangular
+# region within the matrix, find the sum of numbers falling
+# inside the rectangle.
+
+def mrs(arr)
+end
