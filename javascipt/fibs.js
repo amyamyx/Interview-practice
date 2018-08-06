0f1a9b2e316b50e9e5113b7a516cd02e6be8070a
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-06 12:40:30 -0700

    Complete fibs

--- javascipt/fibs.js
+++ javascipt/fibs.js
@@ -4,9 +4,27 @@
 // Solve it both iteratively and recursively.
 
 function fibs(num) {
+  let fib = [1, 1];
+  let newEl, l;
 
-}
+  while (fib.length < num) {
+    l = fib.length;
+    newEl = fib[l - 1] + fib[l - 2]
+    fib.push(newEl);
+  };
 
-function fibs_rec(num) {
+  return fib.slice(0, num);
+};
 
-}
+function fibsRec(num) {
+  if (num <= 2) return [1, 1].slice(0, num);
+
+  let prev = fibsRec(num - 1);
+  let newEl = prev[num - 2] + prev[num - 3];
+
+  return prev.concat([newEl]);
+};
+
+
+console.log(fibsRec(8));
+console.log(fibs(8));
--- ruby/fibs.rb
+++ ruby/fibs.rb
@@ -4,7 +4,23 @@
 # Solve it both iteratively and recursively.
 
 def fibs(num)
+  fib = [1,1]
+  
+  while fib.size < num
+    fib.push(fib[-2, 2].reduce(:+))
+  end
+
+  fib[0, num]
 end
 
 def fibs_rec(num)
+  return [1,1][0, num] if num <= 2
+
+  prev = fibs_rec(num - 1)
+  new_el = prev[-2, 2].reduce(:+)
+
+  prev.push(new_el)
 end
+
+p fibs_rec(8)
+p fibs(8)
