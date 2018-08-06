4da651bce265bb84f8299e8e5375300b3c8002c8
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-06 11:47:52 -0700

    Complete caesar_cipher.js

--- javascipt/caesar_cipher.js
+++ javascipt/caesar_cipher.js
@@ -2,6 +2,25 @@
 // outputs the same letters shifted by that amount in the alphabet. 
 // Assume lowercase and no punctuation. Preserve spaces.
 
-function caesar_cipher(str, shift) {
+function caesarCipher(str, shift) {
+  return str.split(" ").map(word => caesarWord(word, shift)).join(" ");
+};
 
-}
+function caesarWord(word, shift) {
+  let letters = 'abcdefghijklmnopqrstuvwxyz'
+  let uLetters = letters.toUpperCase();
+  letters += uLetters;
+
+  let oldIdx, newIdx;
+  let newWord =  word.split("").map(ch => {
+    oldIdx = letters.indexOf(ch);
+    newIdx = (oldIdx % 26 + shift) % 26;
+    newIdx += oldIdx < 26 ? 0 : 26;
+    
+    return letters[newIdx];
+  }).join("");
+
+  return newWord;
+};
+
+console.log(caesarCipher('abc abc xyz ABC DEF', 1));
