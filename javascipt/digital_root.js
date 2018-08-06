0911cdfdd1abd457b7ad68d17cc9a27f893aebb4
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-08-06 12:20:48 -0700

    Solve digital_root without  string conversion

--- javascipt/digital_root.js
+++ javascipt/digital_root.js
@@ -3,17 +3,31 @@
 // Keep repeating until there is only one digit in the result, called the
 // "digital root". Do not use string conversion within your method.
 
+//string conversion
+// function digitalRoot(num) {
+//   if (num < 10) return num
+
+//   let digits = num.toString().split("").map(ch => parseInt(ch));
+//   let sum = 0;
+
+//   digits.forEach(digit => {
+//     sum += digit;
+//   });
+
+//   return digitalRoot(sum);
+// }
+
 function digitalRoot(num) {
-  if (num < 10) return num
-  
-  let digits = num.toString().split("").map(ch => parseInt(ch));
+  if (num < 10) return num;
+
+  let i = 0;
   let sum = 0;
 
-  digits.forEach(digit => {
-    sum += digit;
-  });
+  while (10 ** i <= num) {
+    sum += parseInt(num % (10 ** (i + 1)) / (10 ** i));
+    i ++;
+  };
 
   return digitalRoot(sum);
-}
-
-console.log(digitalRoot(83));
+};
+console.log(digitalRoot(193));
--- ruby/digital_root.rb
+++ ruby/digital_root.rb
@@ -3,9 +3,22 @@
 # Keep repeating until there is only one digit in the result, called the
 # "digital root". Do not use string conversion within your method.
 
+# string conversion
+# def digital_root(num)
+#   return num if num < 10
+#   digital_root(num.to_s.chars.map(&:to_i).reduce(:+))
+# end
+
 def digital_root(num)
   return num if num < 10
-  digital_root(num.to_s.chars.map(&:to_i).reduce(:+))
+  i = 1
+  sum = 0
+  while 10 ** (i - 1) <= num
+    sum += num % (10 ** i) / (10 ** (i - 1))
+    i += 1
+  end
+
+  return digital_root(sum)
 end
 
-p digital_root(99)
+p digital_root(89)
